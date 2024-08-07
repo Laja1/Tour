ea9d8e3879572f6d1c0170e9af1f9906e7993377
Laja1 <freshost01@gmail.com>
2024-08-07 14:41:09 +0100

    accordion

--- src/components/shared/Acc.tsx
+++ src/components/shared/Acc.tsx
@@ -7,35 +7,47 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { assetsLinks } from '../../assets/assetsLinks';
 
-type labelProps ={
-    labelDescription:string,label:string
-}
+type CustomAccordionProps = {
+  expanded: string | false;
+  handleChange: (panel: string) => (_event: React.SyntheticEvent, isExpanded: boolean) => void;
+  panel: string;
+  labelDescription: string;
+  label: string;
+};
 
-export default function CustomAccordion({labelDescription,label}:labelProps) {
-  const [expanded, setExpanded] = React.useState<string | false>(false);
-
-  const handleChange = (panel: string) => (_event: React.SyntheticEvent, isExpanded: boolean) => {
-    setExpanded(isExpanded ? panel : false);
-  };
-
+export default function CustomAccordion({
+  expanded,
+  handleChange,
+  panel,
+  labelDescription,
+  label,
+}: CustomAccordionProps) {
   return (
     <div>
-      <Accordion sx={{backgroundColor:"#0F1012",paddingY:1 }}  expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-              <AccordionSummary
-                  
-          expandIcon={expanded === 'panel1' ? <RemoveIcon sx={{color:'white'}}/> : <AddIcon sx={{color:'white'}}/>}
-          aria-controls="panel1-content"
-          id="panel1-header"
+      <Accordion
+        sx={{ backgroundColor: "#0F1012", paddingY: 1 }}
+        expanded={expanded === panel}
+        onChange={handleChange(panel)}
+      >
+        <AccordionSummary
+          expandIcon={expanded === panel ? <RemoveIcon sx={{ color: 'white' }} /> : <AddIcon sx={{ color: 'white' }} />}
+          aria-controls={`${panel}-content`}
+          id={`${panel}-header`}
         >
-                  <Typography className='text-[10px] lg:text-lg poppins-semibold text-white font-medium flex-row flex items-center gap-3 space-x-3'><img src={assetsLinks.star} className="size-4"/>{label}</Typography>
+          <Typography
+            sx={{ fontFamily: 'poppins-semibold' }}
+            className='text-[10px] lg:text-lg poppins-semibold text-white font-medium flex-row flex items-center gap-3 space-x-3'
+          >
+            <img src={assetsLinks.star} className="size-4" />
+            {label}
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography className='text-[#fff]  poppins-medium text-[9px]'>
+          <Typography sx={{ fontFamily: 'poppins-medium' }} className='text-[#fff] poppins-medium text-[9px]'>
             {labelDescription}
           </Typography>
         </AccordionDetails>
       </Accordion>
-      
-     </div>
+    </div>
   );
 }
